67d2c5fded4332808a884600d1ef245802361ea4
DominykMcNamara <dominyksmith@gmail.com>
2023-08-31 14:16:40 -0400

    assemble primary dependencies

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+     specPattern: "cypress/components/*.cy.{js,jsx,ts,tsx}",
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+      
+    },
+  },
+});
